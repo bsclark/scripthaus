2027fa4f6f442dcc95ea1905e87e3c4e8f1a4a90
bclark <bclark@tendrilinc.com>
2014-04-01 21:35:16 -0600

    comtrya

--- cloudstack-dns-to-ipa-dns/cloud2ipadns.bash
+++ cloudstack-dns-to-ipa-dns/cloud2ipadns.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
-# From orig clouddns.sh, by uknown
-#     ?D. Jackson - Tendril?
+# From orig clouddns.sh, by Dan Jackson - Tendril
 #
 #  Script to scrape cloud instance name/ip and build a bind zone
 #
@@ -13,12 +12,12 @@
 #
 
 # ENV Vars
-_DEBUG="on"        # on/off
+_DEBUG="off"        # on/off
 BASE=/root/bin
 LOGS=$BASE/csdns-log
 VMFILE=$LOGS/vmfile.txt
 CLOUDAPISCRIPT=$BASE/get-api-cloudstack.bash
-ADMINUSERP=`cat $BASE/.dnsonly`
+ADMINUSERP=`cat $BASE/.dnsonly`             # admin user password in file. chmod 600
 
 # Replace/Update with valid values
 DNSSERVER='<dns server>'
@@ -26,9 +25,32 @@ DNSDOMAIN='<DNS Domain name>'
 EMAIL_USERS='<some email address>'
 ADMINUSER='<IPA user allowed to update, delete, add, query IPA DNS>'
 
+function usage() {
+cat << EOF
+Usage: ${0##*/} <options>
+OPTIONS:
+ -d Debug Messages on/off. Defaults to off"
+ -h Usage/Help
+
+Example: Turn on Debug Messages
+${0##*/} -d
+EOF
+exit 0
+}
+
+# A ':' after an option in the getopts line say the next bit is the argument value
+# and is set with 'SOMEVARIABLE="$OPTARG"'
+while getopts "dh" OPTION_NAME; do
+  case $OPTION_NAME in
+    d) _DEBUG="on";;
+    h|*) usage
+       exit 0
+       ;;
+  esac
+done
+
 # Basic email function
-send_mail ()
-{
+send_mail() {
   for USER in $EMAIL_USERS
   do
     echo $1 | mailx -s "ERROR Aquriring New/Updated DNS from Cloudstack" $USER
@@ -52,7 +74,7 @@ if [ ! -d $LOGS ]; then
 fi
 
 # Make sure all required utils are installed
-CMDS="xmlstarlet $CLOUDAPISCRIPT mailx"
+CMDS="xmlstarlet $CLOUDAPISCRIPT"
 for i in $CMDS
 do
   command -v $i >/dev/null && continue || { echo "ERROR: $i command not found."; exit 1; }
@@ -65,7 +87,7 @@ if [ $? -ne 0 ]; then
 fi
 
 {
-rm -rf $LOGS/$VMFILE
+rm -rf $VMFILE
 
 DEBUG echo "Extract current list of vms and thier IPs"
 
@@ -81,7 +103,7 @@ do
   for VMNAME in $CLOUDDATA
     do
     $CLOUDAPISCRIPT command=listVirtualMachines domainid=$DOMAIN name=$VMNAME details=nics|xmlstarlet sel -T -t -m '///ipaddress' -v '.' -n > $LOGS/$VMNAME
-    echo $VMNAME >> $LOGS/$VMFILE
+    echo $VMNAME >> $VMFILE
     done
 done
 
@@ -89,24 +111,54 @@ for LIST in `cat $VMFILE`
 do
   VMHOSTNAME=$LIST
   VMIP=`cat $LOGS/$LIST`
+  
+  DEBUG echo "vmhostname is " $VMHOSTNAME
+  DEBUG echo "vmip is " $VMIP
 
-  LOOKUP=`host $VMHOSTNAME.$DNSDOMAIN $DNSSERVER|grep address|awk '{ print $4 }'`
-
-  if [ -z $LOOKUP ]; then
-    LOOKUP=EMPTY
+  if [ ! -z $VMHOSTNAME ]; then 
+    LOOKUP=`host $VMHOSTNAME.$DNSDOMAIN|grep -v Using|grep -v Name|grep -v Address|grep -v Aliases|grep -v "^[[:space:]]*$"`
+    DEBUG echo "1 lookup is $LOOKUP"
+  else 
+    LOOKUP="EMPTY"
+    DEBUG echo "1 lookup is " $LOOKUP
   fi
 
-  if [ "$VMIP" == $LOOKUP ]; then
+  if [ ! -z $VMIP ]; then
+    LOOKUPREV=`host $VMIP|grep -v Using|grep -v Name|grep -v Address|grep -v Aliases|grep -v "^[[:space:]]*$"`
+    DEBUG echo "1 lookuprev is " $LOOKUPREV
+  else
+    LOOKUPREV="EMPTY"
+    DEBUG echo "1 lookuprev is " $LOOKUPREV
+  fi
+
+  if [[ ! $LOOKUP =~ .*"not found".* ]]; then
+    LOOKUP=`echo $LOOKUP|awk '{ print $4 }'`
+    DEBUG echo "new lookup is " $LOOKUP
+  else
+    LOOKUP="EMPTY"
+    DEBUG echo "new lookup is " $LOOKUP
+  fi
+
+  if [[ ! $LOOKUPREV =~ .*"not found".* ]]; then
+    LOOKUPREV=`echo $LOOKUPREV|awk -F'.' '{ print $4"."$3"."$2"."$1 }'`
+    DEBUG echo "new lookuprev is " $LOOKUPREV
+  else
+    LOOKUPREV="EMPTY"
+    DEBUG echo "new lookuprev is " $LOOKUPREV
+  fi
+
+  if [ "$VMIP" == "$LOOKUP" ] && [ "$VMIP" == "$LOOKUPREV" ]; then
     echo "NoNeedToAddIt : $VMHOSTNAME.$DNSDOMAIN  $VMIP"
-  elif [ $LOOKUP == "EMPTY" ]; then 
+  elif [ "$LOOKUP" == "EMPTY" ] && [ "$LOOKUPREV" == "EMPTY" ]; then
       if [ ! -z "$VMHOSTNAME" ] && [ ! -z "$VMIP" ]; then
         echo "AddIt : $VMHOSTNAME.$DNSDOMAIN  $VMIP"
         ipa dnsrecord-add $DNSDOMAIN $VMHOSTNAME --a-rec $VMIP --a-create-reverse
       else
         DEBUG echo "VM ($VMHOSTNAME) hostname and/or IP ($VMIP) variable is empty"
       fi
-  elif [ $VMIP != $LOOKUP ]; then
+  elif [ "$LOOKUP" != "EMPTY" ] && [ "$LOOKUPREV" != "EMPTY" ] && [ "$LOOKUP" != "$LOOKUPREV" ]; then
     echo "UpdateNeededForIt : $VMHOSTNAME.$DNSDOMAIN  $VMIP"
+    DEBUG echo "lookup hostname is $LOOKUP    ip is $LOOKUPREV"
     # The idea here is to find what DNS is currently set to, 
     # remove it and add the correct DNS entry.
     OLDVMIP=`ipa dnsrecord-find $DNSDOMAIN $VMHOSTNAME --all|grep "A record:"|awk '{ print $3 }'`
@@ -116,11 +168,21 @@ do
     echo "RemoveIt : $VMHOSTNAME.$DNSDOMAIN  $OLDVMIP"
     ipa dnsrecord-del $DNSDOMAIN $VMHOSTNAME --a-rec $OLDVMIP
     ipa dnsrecord-del $THREEOCTET.in-addr.arpa. $ONEOCTET --ptr-rec $VMHOSTNAME.$DNSDOMAIN
-    ipa dnsrecord-add $DNSDOMAIN $VMHOSTNAME --a-rec $VMIP --a-create-reverse
     echo "AddIt : $VMHOSTNAME.$DNSDOMAIN  $VMIP"
+    ipa dnsrecord-add $DNSDOMAIN $VMHOSTNAME --a-rec $VMIP --a-create-reverse
+  elif [ "$LOOKUP" != "EMPTY" ] && [ "$LOOKUP" != "$LOOKUPREV" ]; then
+    DEBUG echo "....boom....."
+    echo "NoNameinCS-It : $VMHOSTNAME.$DNSDOMAIN  $VMIP doing nothing"
+  elif [ "$LOOKUPREV" != "EMPTY" ] && [ "$LOOKUP" != "$LOOKUPREV" ]; then
+    DEBUG echo "....destoryed......"
+    echo "NoIPinCS-It : $VMHOSTNAME.$DNSDOMAIN  $VMIP doing nothing"
   else
-    echo "!!!!Something Broke!!!!!"
-    send_mail "DNS Adds/Updates not taking place from CS to IPA."
+    DEBUG echo "!!!!Something Broke!!!!!"
+    DEBUG echo "VM is $VMHOSTNAME   IP is $VMIP  lookup is $LOOKUP    lookuprev is $LOOKUPREV"
+    if [ $_DEBUG == "off" ]; then
+      send_mail "DNS Adds/Updates not taking place from CS to IPA."
+    fi
+    exit 1
   fi
 done
 
@@ -138,3 +200,4 @@ done
 # http://stackoverflow.com/questions/16394176/how-to-merge-two-files-consistently-line-by-line
 # http://www.cyberciti.biz/tips/debugging-shell-script.html
 #
+
