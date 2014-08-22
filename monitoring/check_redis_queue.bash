6dda553bf72b3f964138d4684be27bb36073e9fb
bclark <bclark@tendrilinc.com>
2014-08-22 09:26:24 -0600

    check_redis_queue.bash

---
+++ monitoring/check_redis_queue.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Check redis resque ques
+
+QUEUES="remote_query user_analytics analytics mailer"
+
+function check_queue {
+  QUEUE=$(/usr/local/bin/redis-cli -h $1 llen resque:queue:$2 | /usr/bin/awk -F " " '{print $2}')
+  THRESH=1000
+  RESPONSE="$2 QUEUED = $QUEUE"
+
+  if [ $QUEUE -lt $THRESH ]; then
+    RESPONSE="$RESPONSE|$2 OK"
+    echo $RESPONSE
+  else
+    RESPONSE="$RESPONSE|$2 threshold breached"
+    echo $RESPONSE
+    exit 2
+  fi
+}
+
+for qname in $QUEUES
+do
+  check_queue $1 $qname
+done
+
